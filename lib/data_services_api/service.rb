a80ba4c34f53f11fab7ca283462a6f23be0a8d09
Ian Dickinson <i.j.dickinson@gmail.com>
2014-02-14 15:24:54 +0000

    Construct a service instance

---
+++ lib/data_services_api/service.rb
@@ -0,0 +1,12 @@
+module DataServicesApi
+  # Denotes the encapsulated DataServicesAPI service
+  class Service
+    DEFAULT_URL = "http://localhost:8080/dsapi"
+
+    attr_reader :url
+
+    def initialize( config = {} )
+      @url = config[:url] || DEFAULT_URL
+    end
+  end
+end
--- spec/data_services_api/data_services_api_spec.rb
+++ spec/data_services_api/data_services_api_spec.rb
@@ -1,8 +1,20 @@
 require './spec/minitest_helper'
 
+require 'data_services_api/service'
+
 describe "DataServicesAPI", "the data services API" do
-  it "should have a version" do
+  it "should have a semantic version" do
     DataServicesApi::VERSION.must_match /\d+\.\d+\.\d+/
+  end
+
+  it "should be constructable with a default URL" do
+    dsapi = DataServicesApi::Service .new
+    dsapi.url.must_match /localhost:8080\/dsapi/
+  end
+
+  it "should be constructable with a given URL" do
+    dsapi = DataServicesApi::Service .new( url: "foo/bar" )
+    dsapi.url.must_match /foo\/bar/
   end
 
 end
