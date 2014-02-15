ef26992aab9790d2ff46cd86e4f8be01dbc002f3
Ian Dickinson <i.j.dickinson@gmail.com>
2014-02-15 12:41:24 +0000

    Ensure that data_services_api.rb loads required files

--- lib/data_services_api.rb
+++ lib/data_services_api.rb
@@ -1,4 +1,8 @@
 require "data_services_api/version"
+require "data_services_api/service"
+require "data_services_api/dataset"
+require "data_services_api/aspect"
+require "data_services_api/query_generator"
 
 module DataServicesApi
 end
