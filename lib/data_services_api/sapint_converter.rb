a0648b69038dd395cd07c49786ecfa8fe9ceb5e1
bogdan <marc.bogdan96@yahoo.com>
2021-05-07 16:04:08 +0100

    Refactored the converter class
    This commit refactors the converter class and fixes a bug where SAPI would receive multiple searchPath params and return an error response

--- lib/data_services_api/sapint_converter.rb
+++ lib/data_services_api/sapint_converter.rb
@@ -2,134 +2,146 @@
 
 module DataServicesApi
   class SapiNTConverter
-    SANITIZE_SEARCH = lambda { |search_text|
-      search_text
-        .gsub(' AND ', ' ')
-        .gsub('( ', '')
-        .gsub(' )', '')
-    }
-
-    REMOVE_PREFIX = lambda { |value|
-      value.split(':')[1] unless value.slice(0...4) == 'http'
-    }
-
-    VALUE_FILTERS = {
-      '@eq' => lambda { |attribute, value|
-        return "#{attribute}=#{value}" unless value.is_a?(Hash)
-        return "#{attribute}=#{REMOVE_PREFIX.call(value['@id'])}" if value.key?('@id')
-
-        "#{attribute}=#{value['@value']}"
-      },
-      '@oneof' => lambda { |attribute, values|
-        result = ''
-        values.each do |value|
-          result += "#{VALUE_FILTERS['@eq'].call(attribute, value)}&"
-        end
-        result.slice(0...-1)
-      },
-      '@ge' => lambda { |attribute, value|
-        return "mineq-#{attribute}=#{value}" unless value.is_a?(Hash)
-
-        "mineq-#{attribute}=#{value['@value']}"
-      },
-      '@gt' => lambda { |attribute, value|
-        return "min-#{attribute}=#{value}" unless value.is_a?(Hash)
-
-        "min-#{attribute}=#{value['@value']}"
-      },
-      '@le' => lambda { |attribute, value|
-        return "maxeq-#{attribute}=#{value}" unless value.is_a?(Hash)
-
-        "maxeq-#{attribute}=#{value['@value']}"
-      },
-      '@lt' => lambda { |attribute, value|
-        return "max-#{attribute}=#{value}" unless value.is_a?(Hash)
-
-        "max-#{attribute}=#{value['@value']}"
-      },
-      '@search' => lambda { |attribute, value|
-        property = REMOVE_PREFIX.call(value['@property'])
-        search_text = SANITIZE_SEARCH.call(value['@value'])
-        "searchPath=#{attribute}&search-#{property}=#{search_text}"
-      },
-      '@sort' => lambda { |values|
-        result = ''
-        values.each do |value|
-          sort_prop = value.key?('@up') ? "+#{value['@up']}" : "-#{value['@down']}"
-          result += "_sort=#{sort_prop}&"
-        end
-        result.slice(0...-1)
-      },
-      '@count' => lambda { |value|
-        '_count=@id' if value == true
-      },
-      '@limit' => lambda { |value|
-        "_limit=#{value}"
-      },
-      '@offset' => lambda { |value|
-        "_offset=#{value}"
-      },
-      '@and' => lambda { |values|
-        result = ''
-        values.each do |value|
-          attribute = REMOVE_PREFIX.call(value.keys[0])
-          relation = value.values[0].keys[0]
-          json = value.values[0].values[0]
-          result += "#{VALUE_FILTERS[relation].call(attribute, json)}&"
-        end
-        result.slice(0...-1)
-      }
-    }.freeze
-
     def initialize(dsapi_query)
       @dsapi_query = JSON.parse(dsapi_query)
     end
 
     # Converts a DSAPI query to SAPINT query
     def to_sapint_query
-      sapint_query = ''
-      @dsapi_query.each do |key, value|
-        sapint_query += "#{VALUE_FILTERS[key].call(value)}&" if VALUE_FILTERS.key?(key)
-      end
-      sapint_query.slice(0...-1)
-    end
-
-    # Converts SAPINT returned JSON format to DSAPI returned JSON format
-    def to_dsapi_response(sapint_response)
-      dsapi_response = []
-
-      sapint_response['items'].each do |value|
-        dsapi_item = to_dsapi_item(value)
-        dsapi_response.push(dsapi_item)
-      end
-
-      dsapi_response
+      result = @dsapi_query.map do |key, value|
+        sapint_query(key, value)
+      end.compact.join('&')
+      sanitize_result(result)
     end
 
     private
 
-    def to_dsapi_item(sapint_item)
-      dsapi_item = {}
-
-      sapint_item.each do |key, value|
-        dsapi_json = to_dsapi_json(key, value)
-        dsapi_item.merge!(dsapi_json)
+    def sapint_query(key, value)
+      case key
+      when '@sort'
+        sort(value)
+      when '@count'
+        count(value)
+      when '@limit'
+        limit(value)
+      when '@offset'
+        offset(value)
+      when '@and'
+        andd(value)
       end
-
-      dsapi_item
     end
 
-    def to_dsapi_json(sapint_key, sapint_value)
-      dataset_name = data_api.split('/').slice(-1)
-
-      return { sapint_key => sapint_value } if sapint_key == '@id'
-      return { "#{dataset_name}:#{sapint_key}" => sapint_value } unless sapint_value.is_a?(Hash)
-
-      dsapi_json = {}
-      sapint_value.each do |key, value|
-        dsapi_json["#{dataset_name}:#{sapint_key}#{key == '@id' ? '' : key.capitalize}"] = value
+    def sort(values)
+      values.map do |value|
+        sort_prop = value.key?('@up') ? "+#{value['@up']}" : "-#{value['@down']}"
+        "_sort=#{sort_prop}"
       end
-      dsapi_json
+    end
+
+    def count(value)
+      '_count=@id' if value == true
+    end
+
+    def limit(value)
+      "_limit=#{value}"
+    end
+
+    def offset(value)
+      "_offset=#{value}"
+    end
+
+    def andd(values)
+      values.map do |value|
+        attribute = remove_prefix(value.keys[0])
+        rel = value.values[0].keys[0]
+        json = value.values[0].values[0]
+        relation(rel, attribute, json)
+      end
+    end
+
+    def relation(rel, attribute, json)
+      case rel
+      when '@eq'
+        eq(attribute, json)
+      when '@oneof'
+        oneof(attribute, json)
+      when '@ge'
+        ge(attribute, json)
+      when '@gt'
+        gt(attribute, json)
+      when '@le'
+        le(attribute, json)
+      when '@lt'
+        lt(attribute, json)
+      when '@search'
+        search(attribute, json)
+      end
+    end
+
+    def eq(attribute, value)
+      return "#{attribute}=#{value}" unless value.is_a?(Hash)
+      return "#{attribute}=#{remove_prefix(value['@id'])}" if value.key?('@id')
+
+      "#{attribute}=#{value['@value']}"
+    end
+
+    def oneof(attribute, values)
+      values.map do |value|
+        eq(attribute, value)
+      end
+    end
+
+    def ge(attribute, value)
+      return "mineq-#{attribute}=#{value}" unless value.is_a?(Hash)
+
+      "mineq-#{attribute}=#{value['@value']}"
+    end
+
+    def gt(attribute, value)
+      return "min-#{attribute}=#{value}" unless value.is_a?(Hash)
+
+      "min-#{attribute}=#{value['@value']}"
+    end
+
+    def le(attribute, value)
+      return "maxeq-#{attribute}=#{value}" unless value.is_a?(Hash)
+
+      "maxeq-#{attribute}=#{value['@value']}"
+    end
+
+    def lt(attribute, value)
+      return "max-#{attribute}=#{value}" unless value.is_a?(Hash)
+
+      "max-#{attribute}=#{value['@value']}"
+    end
+
+    def search(attribute, value)
+      property = remove_prefix(value['@property'])
+      search_text = sanitize_search(value['@value'])
+      "searchPath=#{attribute}&search-#{property}=#{search_text}"
+    end
+
+    def sanitize_search(search_text)
+      search_text
+        .gsub(' AND ', ' ')
+        .gsub('( ', '')
+        .gsub(' )', '')
+    end
+
+    def remove_prefix(value)
+      value.split(':')[1] unless value.slice(0...4) == 'http'
+    end
+
+    # Removes duplicate searchPaths and constructs JSON from params array
+    def sanitize_result(result)
+      json_result = {}
+      result.split('&').each do |value|
+        param = value.split('=')
+        json_result[param[0]] = [] unless json_result.key?(param[0])
+        json_result[param[0]].push(param[1])
+      end
+      json_result['searchPath'] && json_result['searchPath'] = [json_result['searchPath'].first]
+      json_result
     end
   end
 end
