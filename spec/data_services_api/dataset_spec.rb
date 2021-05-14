8a726d12a4d1bab29ec28c91ec1198824742f42c
bogdan <marc.bogdan96@yahoo.com>
2021-05-14 18:57:44 +0100

    Deleted unnecessary tests
    There are some methods that remain in the gem but only work if a DSAPI URL is provided, so when we provide a SAPINT URL the tests for these methods fail; these tests were removed

--- spec/data_services_api/dataset_spec.rb
+++ spec/data_services_api/dataset_spec.rb
@@ -12,34 +12,6 @@ describe 'DataServiceApi::Dataset' do
     VCR.eject_cassette
   end
 
-  # This feature is currently unavailable from SAPINT
-  # it 'should have an id' do
-  #   _(@dataset.id).wont_be_nil
-  # end
-
-  # This feature is currently unavailable from SAPINT
-  # it 'should have a label' do
-  #   _(@dataset.label).must_match(/UK house price index/)
-  # end
-
-  # This feature is currently unavailable from SAPINT
-  # it 'should have a description' do
-  #   _(@dataset.description).must_match(/A Data Cube of UK house price/)
-  # end
-
-  # This feature is currently unavailable from SAPINT
-  # it 'should describe its own structure as a set of aspects' do
-  #   # VCR.use_cassette( name, :record => :new_episodes) do
-  #   structure = @dataset.structure
-  #   _(structure).wont_be_nil
-  #   _(structure).must_respond_to :size
-
-  #   structure.each do |aspect|
-  #     _(aspect.id).wont_be_nil
-  #   end
-  #   # end
-  # end
-
   it 'should have a reference to the service object' do
     _(@dataset.service).wont_be_nil
     _(@dataset.service.url).wont_be_nil
@@ -51,12 +23,4 @@ describe 'DataServiceApi::Dataset' do
     _(json).wont_be_nil
     _(json.size).must_be :>, 0
   end
-
-  # This feature is currently unavailable from SAPINT
-  # it 'should accept a URI and return an RDF description' do
-  #   uri = 'http://landregistry.data.gov.uk/id/region/south-east'
-  #   json = @dataset.describe(uri)
-  #   _(json).wont_be_nil
-  #   _(json['@graph'][0]['within']).must_include 'http://landregistry.data.gov.uk/id/region/england-and-wales'
-  # end
 end
