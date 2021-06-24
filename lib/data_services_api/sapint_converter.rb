0bd468008b9d6466ce91d7864fae3d69da85d193
bogdan <marc.bogdan96@yahoo.com>
2021-06-24 13:43:37 +0100

    Removing prefix from sort properties

--- lib/data_services_api/sapint_converter.rb
+++ lib/data_services_api/sapint_converter.rb
@@ -33,7 +33,7 @@ module DataServicesApi
 
     def sort(values)
       values.map do |value|
-        sort_prop = value.key?('@up') ? "+#{value['@up']}" : "-#{value['@down']}"
+        sort_prop = value.key?('@up') ? "+#{remove_prefix(value['@up'])}" : "-#{remove_prefix(value['@down'])}"
         ['_sort' => sort_prop]
       end.to_h
     end
