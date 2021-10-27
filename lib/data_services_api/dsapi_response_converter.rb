b9230deac0a72b507ad89986aa1a1b40d8f44e1c
bogdan <marc.bogdan96@yahoo.com>
2021-10-27 15:03:11 +0100

    Returning different responses based on the json mode

--- lib/data_services_api/dsapi_response_converter.rb
+++ lib/data_services_api/dsapi_response_converter.rb
@@ -24,7 +24,11 @@ module DataServicesApi
     end
 
     def to_dsapi_json(sapint_key, sapint_value)
-      dataset_name = @data_api[/[a-z]*$/]
+      # Return different response formats based on the set JSON mode
+      return json_mode_compact(sapint_key, sapint_value) if @json_mode_compact
+
+      json_mode_complete(sapint_key, sapint_value)
+    end
 
       return { sapint_key => sapint_value } if sapint_key == '@id'
       return { "#{dataset_name}:#{sapint_key}" => sapint_value } unless sapint_value.is_a?(Hash)
