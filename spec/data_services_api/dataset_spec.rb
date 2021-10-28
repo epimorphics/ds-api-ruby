ba5d7aac5d0990fa24f315fc79129c394acc8ad7
bogdan <marc.bogdan96@yahoo.com>
2021-10-28 14:19:41 +0100

    Mimic the QueryGenerator class for the query object
    Now testing with the query that's also used by original UKHPI app to test if the API is working

--- spec/data_services_api/dataset_spec.rb
+++ spec/data_services_api/dataset_spec.rb
@@ -18,7 +18,16 @@ describe 'DataServiceApi::Dataset' do
   end
 
   it 'should accept a query and return the result' do
-    query = { 'ukhpi:refRegion' => { '@eq' => { '@id' => 'http://landregistry.data.gov.uk/id/region/somerset' } } }
+    query = Class.new do
+      def terms
+        { '@and' => [{ 'ukhpi:refMonth' => { '@ge' => { :@value => '2019-01', :@type => 'http://www.w3.org/2001/XMLSchema#gYearMonth' } } }, { 'ukhpi:refRegion' => { '@eq' => { :@id => 'http://landregistry.data.gov.uk/id/region/united-kingdom' } } }], '@sort' => [{ '@down' => 'ukhpi:refMonth' }], '@limit' => 1 }
+      end
+
+      def to_json(*_args)
+        terms.to_json
+      end
+    end.new
+
     json = @dataset.query(query)
     _(json).wont_be_nil
     _(json.size).must_be :>, 0
