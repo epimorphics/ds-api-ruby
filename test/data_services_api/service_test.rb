0f3aa960966ad3dfb03fbea88276abf22e344789
Ian Dickinson <i.j.dickinson@gmail.com>
2022-01-27 19:32:38 +0000

    Add Prometheus metrics support
    Addresses #15.
    
    Contributes towards epimorphics/hmlr-linked-data#57
    
    This commit adds support for collecting metrics relating to calls to the
    data API. We add an dependency for an `instrumenter`, which defaults to
    Rails' `ActiveSupport::Notifications`. By publishing events via the
    notifier, apps which include `ds-api-ruby` can be configured to collect
    those events and feed them to a Prometheus store.
    
    While setting up the dependency injection for the instrumenter, I have
    also added a similar hook to inject the logger. This will allow us to
    improve the ability of the logger to emit correctly formulated JSON
    messages.
    
    This change also adds significantly more unit test coverage for the
    elements of `service.rb`.

--- test/data_services_api/service_test.rb
+++ test/data_services_api/service_test.rb
@@ -2,9 +2,37 @@
 
 require './test/minitest_helper'
 
+class MockNotifications
+  attr_reader :instrumentations
+
+  def initialize
+    @instrumentations = []
+  end
+
+  def instrument(*args)
+    @instrumentations << args
+  end
+end
+
+class MockLogger
+  attr_reader :messages
+
+  def initialize
+    @messages = Hash.new { |h, k| h[k] = [] }
+  end
+
+  def info(message, &block)
+    @messages[:info] << [message, block.call]
+  end
+end
+
 describe 'DataServicesAPI::Service', vcr: true do
+  let(:api_url) do
+    ENV['API_URL'] || 'http://localhost:8888'
+  end
+
   before do
-    VCR.insert_cassette name
+    VCR.insert_cassette(name, record: :new_episodes)
     @service = DataServicesApi::Service.new
   end
 
@@ -12,8 +40,78 @@ describe 'DataServicesAPI::Service', vcr: true do
     VCR.eject_cassette
   end
 
+  it 'should return the service URL' do
+    _(DataServicesApi::Service.new(url: 'https://wimbledon.com').url)
+      .must_equal('https://wimbledon.com')
+  end
+
   it 'should find a dataset by name' do
     dataset = @service.dataset('ukhpi')
     _(dataset.data_api).must_match %r{/landregistry/id/ukhpi}
+  end
+
+  it 'should raise if getting a dataset with no name' do
+    _ do
+      @service.dataset(nil)
+    end.must_raise
+  end
+
+  it 'should retrieve JSON with HTTP GET' do
+    service = DataServicesApi::Service.new(url: 'http://localhost:8080')
+    json = service.api_get_json("#{api_url}/landregistry/id/ukhpi", { '_limit' => 1 })
+    _(json).wont_be_nil
+    _(json['meta']).wont_be_nil
+  end
+
+  it 'should instrument an API call' do
+    instrumenter = MockNotifications.new
+
+    DataServicesApi::Service
+      .new(url: api_url, instrumenter: instrumenter)
+      .api_get_json("#{api_url}/landregistry/id/ukhpi", { '_limit' => 1 })
+
+    instrumentations = instrumenter.instrumentations
+    _(instrumentations.size).must_equal 1
+    _(instrumentations.first.first).must_equal 'response.data_api'
+  end
+
+  it 'should instrument a failed API call' do
+    instrumenter = MockNotifications.new
+
+    _ do
+      DataServicesApi::Service
+        .new(url: 'http://localhost:8765', instrumenter: instrumenter)
+        .api_get_json('http://localhost:8765/landregistry/id/ukhpi', { '_limit' => 1 })
+    end.must_raise
+
+    instrumentations = instrumenter.instrumentations
+    _(instrumentations.size).must_equal 1
+    _(instrumentations.first.first).must_equal 'connection_failure.data_api'
+  end
+
+  it 'should instrument a failed API call' do
+    instrumenter = MockNotifications.new
+
+    _ do
+      DataServicesApi::Service
+        .new(url: api_url, instrumenter: instrumenter)
+        .api_get_json("#{api_url}/ceci/nest/pa/un/page", { '_limit' => 1 })
+    end.must_raise
+
+    instrumentations = instrumenter.instrumentations
+    _(instrumentations.size).must_equal 2
+    _(instrumentations[0].first).must_equal 'response.data_api'
+    _(instrumentations[1].first).must_equal 'service_exception.data_api'
+  end
+
+  it 'should log the call to the data API' do
+    logger = MockLogger.new
+
+    DataServicesApi::Service
+      .new(url: api_url, logger: logger)
+      .api_get_json("#{api_url}/landregistry/id/ukhpi", { '_limit' => 1 })
+
+    # @TODO: add specific constraints on received log messages
+    _(logger.messages).wont_be_empty
   end
 end
